c0a02071a67f68028f752c2567cf4fbeb88bd861
Nivja <82000484+Nivja@users.noreply.github.com>
2025-04-26 10:00:32 +1200

    Zyla's bark

---
+++ assets/quests/bark_zyla.lua
@@ -0,0 +1,6 @@
+return {
+   "It’s only three days before the next citizen assembly and we still have not agreed on the refreshments.",
+   "I am all for a feijoa hedge, but did we really need a six hours long meeting about where to plant it?",
+   "I should grab a jar of my famous olives for pizza night.",
+   "I should  get the little runner-bike fixed for my granddaughter’s birthday",
+}
